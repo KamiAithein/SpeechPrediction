4cfe9e2a2a28167df452b79565ca81b35c826d76
KamiAithein <k3nnethwkoepcke@gmail.com>
2020-01-07 22:06:37 -0500

    initial commit

---
+++ src/SpeechPrediction.java
@@ -0,0 +1,24 @@
+public class SpeechPrediction {
+
+    /**
+     * main
+     * @param args not used
+     */
+    public static void main(String[] args){
+        /*
+        sp = new Speech Predictor(instantiating text){
+            dm = new Partial Function(ngram -> word)
+            nextWord = function(word){
+                with(dm){
+                    getBestWord(word)
+                }
+            }
+        }
+        starter word = some word
+        while(<condition>){
+            getBestWord(starter word)
+        }
+
+         */
+    }
+}
