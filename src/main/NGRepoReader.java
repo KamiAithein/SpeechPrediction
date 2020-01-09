f66f883ab2fe3d371295f53b8f2252adbfdb7370
KamiAithein <k3nnethwkoepcke@gmail.com>
2020-01-08 23:40:08 -0500

    implemented abstract method findNGrams in NGRepoReader that finds NGram from a given input string. Created but did not implement NGram

---
+++ src/main/NGRepoReader.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * An NGRepoReader is the output interface for the data stored on disc
+ */
+public abstract class NGRepoReader implements Iterator<NGram> {
+    /**
+     * Finds and returns all ngrams that match the given input
+     * @precondition |input| = |this.File.NGram| - 1
+     * @param input array of strings used to find NGram where NGram[0, |input|) = input
+     * @return a list of NGrams that match the input
+     */
+    public List<NGram> findNGrams(String ... input){
+         List<NGram> ngrams = new ArrayList<>();
+         forEachRemaining(ngram -> {
+             String[] words = ngram.getWords();
+             boolean match = true;
+             for(int i = 0; i < input.length && match; i ++){
+                 if(!words[i].equals(input[i])){
+                     match = false;
+                 }
+             }
+             if(match){
+                 ngrams.add(ngram);
+             }
+         });
+         return ngrams;
+     }
+}
