fd054fd0fcaa8f1ed7a63f8ad58919b003a56559
KamiAithein <k3nnethwkoepcke@gmail.com>
2020-01-14 11:44:12 -0500

    I don't know why I pushed to master when it was 2 am. Described main function so that it is legible, and fixed output to apply to normal sets of data. (For some reason a branch that dealt with experimental input formatting was pushed to master.)

--- src/main/SpeechPrediction.java
+++ src/main/SpeechPrediction.java
@@ -6,34 +6,50 @@ import java.util.function.Consumer;
 public class SpeechPrediction {
 
     /**
+     * Shifts a string left one. Used to iterate through text in the training data.
+     */
+    private static Consumer<String[]> shiftLeft = strings -> {
+        if (strings.length > 1) {
+            System.arraycopy(strings, 1, strings, 0, strings.length - 1);
+        }
+        strings[strings.length - 1] = null;
+    };
+    /**
+     * The starting seed: The phrase that will be built off of. MUST exist within the training data.
+     */
+    private static String[] input = {"and", "people", "of", "the"};
+    /**
+     * The length of the ngram. Stored in byte because it will probably never really be longer than 100...
+     */
+    private static final byte ngramLength = 4;
+    /**
+     * All of the data files to be trained on
+     */
+    private static final File[] trainingData = {new File("data/djt/DJTspeeches0.txt")};
+    /**
      * main
      * @param args not used
      */
     public static void main(String[] args){
-        String[] input = {"i", "can", "make", "it"/*, "5428", "on"*/};
-        for(int i = 0; i < input.length; i ++){
-            System.out.print(input[i]);
+        //Prints the starting seed before prediction
+        for (String s : input) {
+            System.out.print(s);
             System.out.print(" ");
 
         }
-        NGRepo ngp = new HashMapNGRepo((byte)4);
+        NGRepo ngp = new HashMapNGRepo(ngramLength);
         BasicSpeechPredictor bsp = new BasicSpeechPredictor(ngp);
-        bsp.train(new File("data/test_ron.txt"));
-        Consumer<String[]> shiftLeft = strings -> {
-            if (strings.length > 1) {
-                System.arraycopy(strings, 1, strings, 0, strings.length - 1);
-            }
-            strings[strings.length - 1] = null;
-        };
+        for(File f : trainingData){
+            bsp.train(f);
+        }
         for(int i = 0; i < 500; i ++){
             if(i % 6 == 0)
-            System.out.println("");
+                System.out.println("");
             String prediction = bsp.predict(input);
-            System.out.print(prediction.replaceAll("period", "\n"));
             System.out.print(" ");
             shiftLeft.accept(input);
             input[input.length - 1] = prediction;
-
+            System.out.print(prediction);
         }
     }
 }
